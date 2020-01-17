e24d402059c115486e7bb23225fd784dbbb95602
Roberto L. Taborda <rlt802@gmail.com>
2020-01-17 15:46:54 -0500

    base for vpc module

---
+++ AWS/modules/vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "user_tag" {
+  description = "tag to relate the resource to the user that created it"
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
